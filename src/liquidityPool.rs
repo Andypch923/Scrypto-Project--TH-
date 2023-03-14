24b36c963bc5f64a87b7f1d49ef801089df32c36
Andy Poon <andypoon923@gmail.com>
2023-03-14 02:00:49 -0700

    Added Methods
    In CheckCapacity, one error. Will solve Tomorrow.

--- src/liquidityPool.rs
+++ src/liquidityPool.rs
@@ -5,7 +5,8 @@ use scrypto::prelude::*;
     struct liquidty_pool{
         th_vault: Vault,
         xrd_tokens_vault: Vault,
-        price_per_token: Decimal
+        price_per_token: Decimal,
+        locked: bool,
     }
 
     impl liquidty_pool{
@@ -19,7 +20,8 @@ use scrypto::prelude::*;
         Self{
             th_vault: Vault::with_bucket(bucket),
             xrd_tokens_vault: Vault::new(RADIX_TOKEN),
-            price_per_token: price_per_token
+            price_per_token: price_per_token,
+            locked: true
         }
         .instantiate()
         }
@@ -35,15 +37,52 @@ use scrypto::prelude::*;
             let mut component = Self {}. instantiate();
 
             let access_rules = AccessRules::new()
-                .method()
-                .method()
-                .default();
+                .default(access_rules);
 
             component.add_access_check(access_rules);
 
             (component.globalize(),admin_badge,moderator_badge)
         }
 
+        fn lock_Unlock(){
+            if locked = true{
+                let locked = false;
+            }else{
+                let locked = true;
+            }
+        }   
+
+        pub fn withdraw(&mut self, receiver_address:ComponentAddress,receiver_deposit: Bucket){
+            let purchase_amount: Decimal = receiver_deposit.amount()/price_per_token;
+            self.xrd_tokens_vault.put(funds);
+            self.th_vault.take(purchase_amount);
+            receiver_address.th_vault.put(purchase_amount);
+
+            //Add algorithm to check liquidity pool xrd:useful token capacity
+        }
+
+        pub fn checkCapacity() -> (String){
+            let mut message: String::from("XRD Amount: ");  
+            message.push_str(self.xrd_tokens_vault.amount());
+            message.push_str("\n");
+            message.push_str("TH Amount: ");
+            message.push_str(self.th_tokens_vault.amount());
+            (message)
+        }
+
+        fn mintTokens(&mut self,amount: Decimal){
+            self.lock_Unlock();
+            
+            self.lock_Unlock();
+        }
+
+        fn set_exchange_rate(&mut self, amount: Decimal){
+
+        }
+
+        fn checkBadge(user_address: ComponentAddress){
+
+        }
 
     }
 }
